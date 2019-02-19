5bc33f3d81f24a82a1347fa1ca174e2fab84a526
Sebastien Soudan <s.soudan@f5.com>
2019-02-19 09:45:28 -0800

    [+] ...

--- examples/gp_gpflow.py
+++ examples/gp_gpflow.py
@@ -3,7 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.rcParams['figure.figsize'] = (12, 6)
-
 #
 # See http://127.0.0.1:8888/notebooks/src/chapGP/regression.ipynb
 #
@@ -13,13 +12,8 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 import examples.data as data
+import gp.gp_gpflow
 
-# def get_data():
-#     N = 12
-#     X = np.random.rand(N, 1)
-#     Y = np.sin(12 * X) + 0.66 * np.cos(25 * X) + np.random.randn(N, 1) * 0.1 + 3
-#
-#     return X, Y
 
 def evalHandcrafted(X, Y):
     k = gpflow.kernels.Matern52(1, lengthscales=0.3)
@@ -27,60 +21,6 @@ def evalHandcrafted(X, Y):
     m.likelihood.variance = 0.01
 
     return m
-
-def plot(X, Y, x, m, t, f = None):
-
-    # GPflow models have several prediction methods:
-    # * m.predict_f returns the mean and variance of the latent function (f) at the points Xnew.
-    # * m.predict_f_full_cov additionally returns the full covariance matrix of the prediction.
-    # * m.predict_y returns the mean and variance of a new data point (i.e. includes the noise varaince). In the case of non-Gaussian likelihoods, the variance is computed by (numerically) integrating the non-Gaussian likelihood.
-    # * m.predict_f_samples returns samples of the latent function
-    # * m.predict_density returns the log-density of the points Ynew at Xnew.
-
-    mean, var = m.predict_y(x)
-    plt.figure(figsize=(12, 6))
-    plt.plot(X, Y, 'kx', mew=2)
-    plt.plot(x, mean, 'C0', lw=2)
-    if f is not None:
-        plt.plot(x, f(x), 'k', lw=2)
-    plt.fill_between(x[:, 0],
-                     mean[:, 0] - 2 * np.sqrt(var[:, 0]),
-                     mean[:, 0] + 2 * np.sqrt(var[:, 0]),
-                     color='C0', alpha=0.2)
-    plt.xlim(-0.1, 1.1)
-    plt.title(t)
-    plt.show()
-
-
-def evalMLE(X, Y):
-    k = gpflow.kernels.Matern52(1, lengthscales=0.3)
-    meanf = gpflow.mean_functions.Linear(1.0, 0.0)
-    m = gpflow.models.GPR(X, Y, k, meanf)
-    m.likelihood.variance = 0.01
-
-    print(m.as_pandas_table())
-
-    gpflow.train.ScipyOptimizer().minimize(m)
-    return m
-
-def evalMCMC(X, Y):
-    k = gpflow.kernels.Matern52(1, lengthscales=0.3)
-    meanf = gpflow.mean_functions.Linear(1.0, 0.0)
-    m = gpflow.models.GPR(X, Y, k, meanf)
-    m.clear()
-
-    m.kern.lengthscales.prior = gpflow.priors.Gamma(1., 1.)
-    m.kern.variance.prior = gpflow.priors.Gamma(1., 1.)
-    m.likelihood.variance.prior = gpflow.priors.Gamma(1., 1.)
-    m.mean_function.A.prior = gpflow.priors.Gaussian(0., 10.)
-    m.mean_function.b.prior = gpflow.priors.Gaussian(0., 10.)
-    m.compile()
-    print(m.as_pandas_table())
-
-    sampler = gpflow.train.HMC()
-    samples = sampler.sample(m, num_samples=2000, burn=1000, epsilon=0.05, lmin=1, lmax=20,
-                             logprobs=False)
-    return samples, m
 
 
 if __name__ == '__main__':
@@ -91,17 +31,17 @@ if __name__ == '__main__':
     plt.show()
 
     m1 = evalHandcrafted(X, Y)
-    plot(X, Y, x, m1, 'handcrafted GP model', f)
+    gp.gp_gpflow.plot(X, Y, x, m1, 'handcrafted GP model', f)
     print(m1.as_pandas_table())
     m1.clear()
 
-    m2 = evalMLE(X, Y)
-    plot(X, Y, x, m2, 'MLE-fitted model', f)
+    _, m2 = gp.gp_gpflow.evalMLE(X, Y)
+    gp.gp_gpflow.plot(X, Y, x, m2, 'MLE-fitted model', f)
     print(m2.as_pandas_table())
     m2.clear()
 
-    traces, m3 = evalMCMC(X, Y)
-    plot(X, Y, x, m3, 'MCMC-fitted model', f)
+    traces, m3 = gp.gp_gpflow.evalMCMC(X, Y)
+    gp.gp_gpflow.plot(X, Y, x, m3, 'MCMC-fitted model', f)
     print(m3.as_pandas_table())
 
     plt.figure(figsize=(8, 4))
--- examples/gp_sklearn.py
+++ examples/gp_sklearn.py
@@ -1,101 +1,23 @@
-
 import numpy as np
-from matplotlib import pyplot as plt
-
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel as C
 
 import examples.data as data
+import gp.gp_sklearn
 
-#
-# See https://scikit-learn.org/stable/modules/gaussian_process.html
-# See https://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpr_noisy_targets.html
-#
-
-def evalMLENoiseless(X, Y, x, f = None):  # type: (Any, Any, Any, Any) -> None
-
-    # Instantiate a Gaussian Process model
-    kernel = C(1.0, (1e-4, 1e4)) * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3))
-
-    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
-
-    # Fit to data using Maximum Likelihood Estimation of the parameters
-    gp.fit(X, Y)
-
-    # Make the prediction on the meshed x-axis (ask for MSE as well)
-    y, sigma = gp.predict(x, return_std=True)
-
-    # Plot the function, the prediction and the 95% confidence interval based on
-    # the MSE
-    plt.figure()
-    if f is not None:
-        plt.plot(x, f(x), 'r:', label=u'$f(x) = x\,\sin(x)$')
-    plt.plot(X, Y, 'r.', markersize=10, label=u'Observations')
-    plt.plot(x, y, 'b-', label=u'Prediction')
-    plt.fill(np.concatenate([x, x[::-1]]),
-             np.concatenate([y - 1.9600 * sigma,
-                            (y + 1.9600 * sigma)[::-1]]),
-             alpha=.5, fc='b', ec='None', label='95% confidence interval')
-    plt.xlabel('$x$')
-    plt.ylabel('$f(x)$')
-    # plt.ylim(-10, 20)
-    plt.legend(loc='upper left')
-
-def evalMLENoisy(X, Y, x, f = None, DY = 0):  # type: (Any, Any, Any, Any) -> None
-
-    # Instantiate a Gaussian Process model
-    kernel = C(1.0, (1e-4, 1e4)) * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3)) \
-             + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e+1))
-
-    # Instantiate a Gaussian Process model
-    gp = GaussianProcessRegressor(kernel=kernel, alpha=DY ** 2,
-                                  n_restarts_optimizer=10)
-
-    # Fit to data using Maximum Likelihood Estimation of the parameters
-    gp.fit(X, Y)
-
-    # Make the prediction on the meshed x-axis (ask for MSE as well)
-    y, sigma = gp.predict(x, return_std=True)
-
-    # Plot the function, the prediction and the 95% confidence interval based on
-    # the MSE
-    plt.figure()
-    if f is not None:
-        plt.plot(x, f(x), 'r:', label=u'$f(x) = x\,\sin(x)$')
-    plt.errorbar(X.ravel(), Y, DY, fmt='r.', markersize=10, label=u'Observations')
-    plt.plot(x, y, 'b-', label=u'Prediction')
-    plt.fill(np.concatenate([x, x[::-1]]),
-             np.concatenate([y - 1.9600 * sigma,
-                            (y + 1.9600 * sigma)[::-1]]),
-             alpha=.5, fc='b', ec='None', label='95% confidence interval')
-    plt.xlabel('$x$')
-    plt.ylabel('$f(x)$')
-    # plt.ylim(-10, 20)
-    plt.legend(loc='upper left')
-    plt.show()
 
 if __name__ == '__main__':
-    # f =  lambda x: x * np.sin(x)
-    # # ----------------------------------------------------------------------
-    # #  First the noiseless case
-    # X = np.atleast_2d([1., 3., 5., 6., 7., 8.]).T
-    #
-    # # Observations
-    # Y = f(X).ravel()
-    #
-    # # Mesh the input space for evaluations of the real function, the prediction and
-    # # its MSE
-    # x = np.atleast_2d(np.linspace(0, 10, 1000)).T
     X, Y, x, f = data.make_data()
 
-    evalMLENoiseless(X, Y, x, f)
+    y, sigma = gp.gp_sklearn.evalMLENoiseless(X, Y, x)
+    gp.gp_sklearn.plot(X, Y, x, y, sigma, DY=None, f=f)
 
     # ----------------------------------------------------------------------
     # now the noisy case
 
     # Observations and noise
-    DY = 0.25 + 0.5 * np.random.random(Y.shape)
+    dy = 0.5
+    DY = dy * np.random.random(Y.shape)
     noise = np.random.normal(0, DY)
     Y += noise
 
-    evalMLENoisy(X, Y, x, f, DY)
+    y, sigma = gp.gp_sklearn.evalMLENoisy(X, Y, x, dy)
+    gp.gp_sklearn.plot(X, Y, x, y, sigma, DY, f)
