ddb6d6611b7b87f045e80b307197116e79972b28
Sebastien Soudan <s.soudan@f5.com>
2019-02-27 16:22:43 -0800

    [+] build script

--- check.sh
+++ check.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "----------TODO-------------"
+grep -r 'TODO(' * | grep -v grep | sed 's/.*TODO/TODO/'
+
 echo "----------LINT-------------"
 pylama -l "pycodestyle,mccabe"
 echo "----------OK---------------"
